913d3396895d1c47d63caacaa54336b4098bc719
Lavizor <72126847+Lavizor@users.noreply.github.com>
2025-10-23 22:00:44 +0200

    Soundmanager source system

--- Assets/PlaySoundEnter.cs
+++ Assets/PlaySoundEnter.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class PlaySoundEnter : StateMachineBehaviour
 {
     [SerializeField] private SoundType sound;
-    [SerializeField] private AudioSource source = null;
+    [SerializeField] private AudioSourceName source = AudioSourceName.Main_Camera;
     [SerializeField,Range(0,1)] private float volume = 1f;
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
