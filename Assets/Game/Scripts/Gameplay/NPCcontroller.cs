ddc14e8df5cde910e42f93cffe86d772e2b80197
V-the-dev <vherre06@ucm.es>
2025-10-25 01:05:47 +0200

    NPCcontroller now admit a list of dialogueObject (and works)

--- Assets/Game/Scripts/Gameplay/NPCcontroller.cs
+++ Assets/Game/Scripts/Gameplay/NPCcontroller.cs
@@ -1,26 +1,48 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class NPCcontroller :  InteractableObject
 {
-    //[SerializeField] private DialogueObject interactionDialogue;
+    [SerializeField] private List<DialogueObject> interactionDialogues;
+    [SerializeField] private List<DialogueObject> proximityDialogues;
+    InventoryDialogueLinker inventoryDialogueLinker;
 
-    [SerializeField] private DialogueObject interactionDialogue;
-    [SerializeField] private DialogueObject proximityDialogue;
-
-    override public void  Trigger()
+    void Awake()
     {
-        MessageManager.Instance.dialogueActivator.UpdateDialogueObject(interactionDialogue);
-
-        MessageManager.Instance.Interact();
-
-        Debug.Log("trigger");
+        inventoryDialogueLinker = InventoryManager.Instance.GetComponent<InventoryDialogueLinker>();
+        //if(transform.GetChild)
     }
 
-    override public void AutoTrigger()
+    public override void Trigger()
     {
-        MessageManager.Instance.dialogueActivator.UpdateDialogueObject(proximityDialogue);
-
-        MessageManager.Instance.Interact();
+        if (interactionDialogues.Count <= 0) return;
+        if (!inventoryDialogueLinker) return;
+        foreach (DialogueObject interactionDialogue in interactionDialogues)
+        {
+            if (inventoryDialogueLinker.CanShowDialogue(interactionDialogue) == true)
+            {
+                MessageManager.Instance.dialogueActivator.UpdateDialogueObject(interactionDialogue);
+                MessageManager.Instance.Interact();
+                break;
+            }
+        }
     }
+
+    public override void AutoTrigger()
+    {
+        if (proximityDialogues.Count <= 0) return;
+        if (!inventoryDialogueLinker) return;
+        foreach (DialogueObject proximityDialogue in proximityDialogues)
+        {
+            if (inventoryDialogueLinker.CanShowDialogue(proximityDialogue) == true)
+            {
+                MessageManager.Instance.dialogueActivator.UpdateDialogueObject(proximityDialogue);
+                MessageManager.Instance.Interact();
+                break;
+            }
+        }
+    }
+
+
 
 }
